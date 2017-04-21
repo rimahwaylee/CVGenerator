d1d7c992b2be34ab38767a208263d88c5340184e
Rima <rima.waylee@gmail.com>
2017-04-21 16:03:47 +0100

    template changes

---
+++ FrondEnd/src/app/templates/template2/viewtemplate2.component.ts
@@ -0,0 +1,18 @@
+/**
+ * Created by Rima on 4/5/2017.
+ */
+
+
+import {Component} from "@angular/core";
+
+
+@Component({
+  selector :   'app-viewtemplate2',
+  templateUrl :'viewtemplate2.component.html',
+  styleUrls : ['viewtemplate2.component.css']
+})
+
+export class Viewtemplate2Component {
+  pageTitle : string = 'viewtemplate2'
+}
+
---
+++ FrondEnd/src/app/templates/template3/viewtemplate3.component.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by Rima on 4/5/2017.
+ */
+
+
+import {Component} from "@angular/core";
+
+
+@Component({
+  selector :   'app-viewtemplate3',
+  templateUrl :'viewtemplate3.component.html',
+  styleUrls : ['viewtemplate3.component.css']
+})
+
+export class Viewtemplate3Component {
+  pageTitle : string = 'viewtemplate3'
+}
